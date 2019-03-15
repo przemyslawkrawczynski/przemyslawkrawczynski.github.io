6f18de6b438e3ab910bf80aa200246ac4ba28676
pkrawczynski <pk@bluzo.pl>
2019-03-15 09:01:04 +0100

    Delete Update

--- script.js
+++ script.js
@@ -66,12 +66,12 @@ $(document).ready(function() {
     function handleTaskDeleteRequest() {
       var parentEl = $(this).parent().parent();
       var taskId = parentEl.attr('data-task-id');
-      var requestUrl = apiRoot + 'deleteTask';
+      var requestUrl = apiRoot + 'deleteTaskById/' + taskId;
+     
+	
   
       $.ajax({
-        url: requestUrl + '/?' + $.param({
-          taskId: taskId
-        }),
+        url: requestUrl,
         method: 'DELETE',
         success: function() {
           parentEl.slideUp(400, function() { parentEl.remove(); });
