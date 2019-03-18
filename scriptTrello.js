e215890a8bbd2291a3396795e977b96ec0dae150
pkrawczynski <pk@bluzo.pl>
2019-03-18 15:04:24 +0100

    Upgrade trello js.

--- scriptTrello.js
+++ scriptTrello.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://protected-waters-63381.herokuapp.com/';
+  const apiRoot = 'https://protected-waters-63381.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://protected-waters-63381.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
